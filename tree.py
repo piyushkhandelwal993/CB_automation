2cf0db996bc5479d93fdcc5a632569392eeda1f1
Abdur Rahman <anvg17@gmail.com>
2015-06-12 15:28:10 +0530

    Updating tree

--- tree.py
+++ tree.py
@@ -6,13 +6,22 @@ class node:
         self.properties={}
         self.children=[]
 
+    def __init__(self,t,h,ps,prop,children):
+        self.type=t
+        self.hash=h
+        self.psource=ps
+        self.properties=prop
+        self.children=children
+
     def add_child(self,child):
         self.children.append(child)
 
     def no_of_childs(self):
         return len(self.children)
 
-
+    def add_property(self,a,b):
+        self.properties[a]=b
+        
 
 class tree:
 
@@ -22,21 +31,17 @@ class tree:
     def __init__(self,root):
         self.root=root
         
-
-def dfs(t):
-    if(t==None): return
-    elif (t.root==None) return
-    else dfs_node_level(t.root)
-
 def dfs_node_level(node):
     if(node==None) return 
     for i in node.children:
         print (i)
         dfs_node_level(i)
-    pass
+    
 
-
+def dfs(t):
+    if(t==None): return
+    elif (t.root==None) return
+    else dfs_node_level(t.root)
+    
     
 b=node()
-node=3
-print (node)
